6e0d6bd1dca6980e88289e1693f3021c38914aaf
Borys <boris.feldsher@live.com>
2025-05-01 15:37:42 +0200

    Added Error handling

---
+++ Models/TagGetResult.cs
@@ -0,0 +1,26 @@
+﻿namespace BlazorApp.Models
+{
+    public class TagGetResult
+    {
+        public bool IsSuccess { get; set; }
+        public List<Tag>? Tags { get; set; }
+        public string? ErrorMessage { get; set; }
+
+        public static TagGetResult Success(List<Tag> tags)
+        {
+            return new TagGetResult
+            {
+                IsSuccess = true,
+                Tags = tags
+            };
+        }
+        public static TagGetResult Fail(string message)
+        {
+            return new TagGetResult
+            {
+                IsSuccess = false,
+                ErrorMessage = message
+            };
+        }
+    }
+}
